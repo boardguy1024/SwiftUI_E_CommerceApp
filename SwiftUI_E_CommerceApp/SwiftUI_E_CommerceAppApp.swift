05774f4b985966009745ddeffa12e77544605b1e
boardguy1024 <boardguy1024@gmail.com>
2021-08-15 21:20:07 +0900

    Initial Commit

---
+++ SwiftUI_E_CommerceApp/SwiftUI_E_CommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_E_CommerceAppApp.swift
+//  SwiftUI_E_CommerceApp
+//
+//  Created by park kyung seok on 2021/08/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_E_CommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
